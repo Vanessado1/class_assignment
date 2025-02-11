8ea4da563f1daf096dc7a97dad42b35ef0ae9b11
Vanessa Do <xex.vanessa@gmail.com>
2025-02-11 16:05:46 -0500

    Completed Task 1

--- cc_example.js
+++ cc_example.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+// Task 1 
+let products =[
+   {name: "Laptop", price: 1200, category: "Electronics" },
+   {name: "notebook", price: 5, category: "Stationery"},
+   {name: "Headphones", price: 100, category: "Electronics"},
+];
+
+// function getProductsByCategory(products, category) {
+//     return products.filter(product => product.category === category);
+// }
+// console.log(getProductsByCategory(products,"Electronics")); 
+
+// Task 2 
+
