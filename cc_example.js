75d6170b820bb2817666b3c4c10555b072faa307
Vanessa Do <xex.vanessa@gmail.com>
2025-02-11 16:37:08 -0500

    completed task 4

--- cc_example.js
+++ cc_example.js
@@ -25,12 +25,20 @@ let products =[
 
 // Task 3 
 
-let sales = [250,400,150,900,1200];
+// let sales = [250,400,150,900,1200];
 
-function calculateTotalRevenue(sales) {
-    return sales.reduce((total, sale) =>total+sale,0)
+// function calculateTotalRevenue(sales) {
+//     return sales.reduce((total, sale) =>total+sale,0)
+// }
+
+// console.log(`Total Rev: $${calculateTotalRevenue(sales)}`);
+
+// Task 4 
+
+let employee = {name: "John Doe", salary: 50000, position: "Manager"};
+
+function updateSalary(employee, percentageIncrease) {
+    return employee.salary += employee.salary * percentageIncrease;
 }
 
-console.log(`Total Rev: $${calculateTotalRevenue(sales)}`);
-
-//Task 4
+console.log(updateSalary(employee, 0.1));
